64d2420f3b652b2c2ed944a429e69da28cd08427
Tebello Maila <maila.mtebello@gmail.com>
2022-09-11 13:51:44 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - print: Programming is like building a multilingual puzzle
+ *
+ * Return: Always 0 (Success). Otherwise -1 (Fail)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}

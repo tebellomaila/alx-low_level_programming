d0bb54b43ac72cf70f2833ed97c054d382a4dc3f
Tebello Maila <maila.mtebello@gmail.com>
2022-09-13 12:43:59 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Print _putchar
+ *
+ * Return: string value (Success).
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}

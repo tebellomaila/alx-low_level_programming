097e9fbc1f50446397a0e911319f4070866fdeb1
Tebello Maila <maila.mtebello@gmail.com>
2022-09-19 17:32:20 -0700

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - update the value of integer pointer to 98
+ *
+ * @n: integer
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _putchar - writes character to standard output
+ *
+ * @c: character
+ *
+ * Return: void
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}

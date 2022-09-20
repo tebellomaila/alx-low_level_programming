5ee52f4c4e3fc9d91c1362c18b38d704c92f26bc
Tebello Maila <maila.mtebello@gmail.com>
2022-09-19 18:11:27 -0700

    compiled 98 Battery st.

--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <unistd.h>
 
 /**
  * _putchar - writes character to standard output
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,16 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char *);
-void reset_to_98(int);
-void swap_int(int, int);
-int _strlen(char *);
-void _puts(char *);
-void print_rev(char *);
-void rev_string(char *);
-void puts2(char *)
-void puts_half(char *);
-void print_array(int *, int);
-char *_strcpy(char *, char *);
+int _putchar(char c);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(int *a, int *b);
 
 #endif /* MAIN_H*/

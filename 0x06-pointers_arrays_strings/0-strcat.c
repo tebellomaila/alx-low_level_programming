73d447c9b5683e73ba4e0408f104067bfdfb0e7f
Tebello Maila <maila.mtebello@gmail.com>
2022-09-22 12:39:14 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,34 @@
+
+/**
+ * *_strcat - concatenates the two strings
+ *
+ * @dest: the pointer to char dest
+ * @src: the pointer to char src
+ *
+ * Return: void
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	i = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	j = 0;
+
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
